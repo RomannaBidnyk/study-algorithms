cffc8d350a31a87961d2c96b31c32a69bf44b092
RomannaBidnyk <romanna.bidnyk@gmail.com>
2022-03-31 01:10:43 +0300

    1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ src/main/java/leetcode/array/easy/AverageSalaryExcludingMinimumAndMaximum_1491.java
@@ -0,0 +1,23 @@
+package leetcode.array.easy;
+
+public class AverageSalaryExcludingMinimumAndMaximum_1491 {
+
+    /**
+     * Time: O(n)
+     * Space: O(1)
+     */
+
+    public static double average(int[] salary) {
+        int divideNumber = salary.length - 2;
+        int min = salary[0];
+        int max = salary[0];
+
+        int sumAll = 0;
+        for (int i : salary) {
+            if (i < min) min = i;
+            if (i > max) max = i;
+            sumAll += i;
+        }
+        return (double) (sumAll - min - max) / divideNumber;
+    }
+}
