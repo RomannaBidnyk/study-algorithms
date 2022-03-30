066a00a7a2defd2ab7e1d8400968f8d2501446d4
romanna <romashkavpoly1@gmailc.om>
2022-03-31 01:06:45 +0300

    1523. Count Odd Numbers in an Interval Range

---
+++ src/main/java/leetcode/math/easy/CountOddNumbersInAnIntervalRange_1523.java
@@ -0,0 +1,25 @@
+package leetcode.math.easy;
+
+public class CountOddNumbersInAnIntervalRange_1523 {
+
+    /**
+     * Time: O(1)
+     * Space: O(1)
+     */
+
+    public static int countOdds(int low, int high) {
+        int count = 0;
+        if (low % 2 == 1) {
+            count++;
+            low++;
+        }
+        if (high % 2 == 1) {
+            count++;
+            high--;
+        }
+
+        int diff = high - low;
+        count += diff / 2;
+        return count;
+    }
+}
