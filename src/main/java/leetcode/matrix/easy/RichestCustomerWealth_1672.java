20653b3be28fd2caf221769de59b941a13b62a4b
romanna <romashkavpoly1@gmailc.om>
2022-03-31 01:00:15 +0300

    1672. Richest Customer Wealth

---
+++ src/main/java/leetcode/matrix/easy/RichestCustomerWealth_1672.java
@@ -0,0 +1,21 @@
+package leetcode.matrix.easy;
+
+public class RichestCustomerWealth_1672 {
+
+    /**
+     * Time: O(M*N) - M - the number of customers and N - the number of banks.
+     * Space: O(1)
+     */
+
+    public static int maximumWealth(int[][] accounts) {
+        int maxWealth = Integer.MIN_VALUE;
+        for (int i = 0; i < accounts.length; i++) {
+            int currSum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                currSum += accounts[i][j];
+            }
+            if (currSum > maxWealth) maxWealth = currSum;
+        }
+        return maxWealth;
+    }
+}
