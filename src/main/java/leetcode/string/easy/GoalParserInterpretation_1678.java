fb0e28bf144ef6db308a56acddeeea6c25275974
romanna <romashkavpoly1@gmailc.om>
2022-04-01 00:56:00 +0300

    1678. Goal Parser Interpretation

---
+++ src/main/java/leetcode/string/easy/GoalParserInterpretation_1678.java
@@ -0,0 +1,22 @@
+package leetcode.string.easy;
+
+public class GoalParserInterpretation_1678 {
+
+    public static String interpret(String command) {
+        StringBuilder result = new StringBuilder();
+        char[] letters = command.toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] == 'G') result.append("G");
+            else if (letters[i] == '(') {
+                if (letters[i + 1] == ')') result.append("o");
+                else result.append("al");
+            }
+        }
+        return result.toString();
+    }
+
+    public static String interpret2(String command) {
+        return command.replace("()", "o").replace("(al)", "al");
+    }
+
+}
