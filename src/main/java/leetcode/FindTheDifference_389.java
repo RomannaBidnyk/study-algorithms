1763e70f9e9cc9da41d215c7900e66ee6fe0f56b
RomannaBidnyk <romanna.bidnyk@gmail.com>
2022-03-31 17:28:39 +0300

    389. Find the Difference

---
+++ src/main/java/leetcode/FindTheDifference_389.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindTheDifference_389 {
+
+    /**
+     * v1 Set
+     */
+    public static char findTheDifference(String s, String t) {
+        Set<Character> set = new HashSet<>();
+        for(char letter : (s+t).toCharArray()){
+            if(!set.add(letter)){
+                set.remove(letter);
+            }
+        }
+        return set.iterator().next();
+    }
+
+    //TODO diff approaches
+}
